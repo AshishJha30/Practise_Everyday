8e16eab2126b83bf96099b2a46889476f1d8e0b9
Ashish Jha <67825558+AshishJha30@users.noreply.github.com>
2021-09-12 23:12:24 +0530

    Add files via upload

---
+++ CheetSheets/Beginner Problems/Basic/Q5_findSize.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << sizeof(int)<< endl; 
+    cout << sizeof(float) << endl; 
+    cout << sizeof(double) << endl; 
+    cout << sizeof(char) << endl; 
+    return 0;
+}
---
+++ CheetSheets/Beginner Problems/Basic/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+ int n1, n2, temp;
+//     cin >> n1 >> n2;
+//     temp = n1;
+//     n1 = n2;
+//     n2 = temp;
+//     cout << "After swapping" << endl;
+//     cout << n1 << " " << n2 << endl;
+//     return 0;
+// }
