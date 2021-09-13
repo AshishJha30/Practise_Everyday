cf85743628d3a3bc07e3305997d0174a2d2f32f3
Ashish Jha <67825558+AshishJha30@users.noreply.github.com>
2021-09-13 22:17:42 +0530

    Add files via upload

---
+++ CheetSheets/Beginner Problems/Loops/Q6_cal_GCD_HCF.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1, n2, hcf;
+    cout << "Enter two numbers: ",
+    cin >> n1 >> n2;
+
+    // if (n2 > n1) {
+    //     int temp = n1;
+    //     n1 = n2;
+    //     n2 = temp;
+    // }
+
+    // for (int i = 1; i <= n2; i++) {
+    //     if (n1 % i == 0 && n2 % i == 0) {
+    //         hcf = i;
+    //     }
+    // }
+    // cout << "HCF = " << hcf;
+
+    while (n1 != n2) {
+        if (n1 > n2) {
+            n1 -= n2;
+        }
+        else {
+            n2 -= n1;
+        }
+    }
+    cout << "HCF = " << n1;
+
+    return 0;
+}
